f33cf65bd67466e15dcadb761a6d8ee1087588e2
GentleMizt <olamideiyanda18@gmail.com>
2023-04-13 12:43:18 +0100

    progress

--- lancer.js
+++ lancer.js
@@ -1,5 +1,11 @@
 'use strict';
 
+// Selecting HTML elements using the DOM
 const p1Score = document.getElementById('score--0');
 const p2Score = document.getElementById('score--1');
+const diceImg = document.querySelector('.dice');
+
+p1Score.textContent = 0;
+p2Score.textContent = 0;
+
 
