1b75b707027252df8f97bb2646df53c6a575ee57
GentleMizt <olamideiyanda18@gmail.com>
2023-04-13 13:09:08 +0100

    progress

--- lancer.js
+++ lancer.js
@@ -3,11 +3,12 @@
 // Selecting HTML elements using the DOM
 const p1Score = document.getElementById('score--0');
 const p2Score = document.getElementById('score--1');
+const p1Current = document.getElementById('current--0');
+const p2Current = document.getElementById('current--1');
 const diceImg = document.querySelector('.dice');
-const btnRoll = document.querySelector('.btn--roll')
-const btnNew = document.querySelector('.btn--new')
-const btnHold = document.querySelector('.btn--hold')
-
+const btnRoll = document.querySelector('.btn--roll');
+const btnNew = document.querySelector('.btn--new');
+const btnHold = document.querySelector('.btn--hold');
 
 // Game Starting Conditions
 p1Score.textContent = 0;
@@ -15,21 +16,23 @@ p2Score.textContent = 0;
 // Hiding the dice at the start of the game.
 diceImg.classList.add('hidden');
 
+let currentScore = 0;
+
 // ROLLING THE DICE
-btnRoll.addEventListener('click', function(){
-    // 1. Generating a random dice
-    const dice = Math.trunc(Math.random() * 6) + 1;
+btnRoll.addEventListener('click', function () {
+  // 1. Generating a random dice
+  const dice = Math.trunc(Math.random() * 6) + 1;
 
-    // 2. Display dice
-    diceImg.classList.remove('hidden');
-    diceImg.src = `imgs/dice-${dice}.png`
+  // 2. Display dice
+  diceImg.classList.remove('hidden');
+  diceImg.src = `imgs/dice-${dice}.png`;
 
-    // 3. Check for rolled 1. switch to next player.
-    if (dice !== 1) {
-        // Add dice to current score
-        
-    } else{
-        // switch to next player.
-    }
+  // 3. Check for rolled 1. switch to next player.
+  if (dice !== 1) {
+    // Add dice to current score
+    currentScore += dice;
+    p1Current.textContent = currentScore; // CHANGE LATER 
+  } else {
+    // switch to next player.
+  }
 });
-
